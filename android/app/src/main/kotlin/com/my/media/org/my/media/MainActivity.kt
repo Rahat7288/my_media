a8f833e2b75451401fbebce430000d26f1420ee4
Rahat Islam Akash <rahatislamakash@gmail.com>
2025-02-16 01:49:11 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/my/media/org/my/media/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.my_media.org.my_media
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:my_media/app/app.dart';
+import 'package:my_media/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
